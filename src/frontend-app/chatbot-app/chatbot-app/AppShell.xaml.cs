c4fea5c27c2d0840e2c72f98b6a0f25eea50ca1e
Hadookhan <hadikhan05@hotmail.co.uk>
2025-11-30 12:22:47 +0000

    initial commit

---
+++ src/frontend-app/chatbot-app/chatbot-app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace chatbot_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/frontend-app/chatbot-app/chatbot-app/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "chatbot_app")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "chatbot_app.Pages")]
